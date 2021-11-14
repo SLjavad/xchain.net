b4f5ba8f13ef12773672fd4629ca48f8bc26afda
SLJAVADPC\SLjavad <jd.mn98@yahoo.com>
2021-11-14 23:34:50 +0330

    added TxHistoryResponse , CosmosSDKClientParams

---
+++ XchainDotnet.Cosmos/Models/CosmosSDKClientParams.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace XchainDotnet.Cosmos.Models
+{
+    public class CosmosSDKClientParams
+    {
+        [JsonPropertyName("server")]
+        public string Server { get; set; }
+        [JsonPropertyName("chainId")]
+        public string ChainId { get; set; }
+        [JsonPropertyName("prefix")]
+        public string Prefix { get; set; }
+    }
+}
---
+++ XchainDotnet.Cosmos/Models/Tx/TxHistoryResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace XchainDotnet.Cosmos.Models.Tx
+{
+    public class TxHistoryResponse
+    {
+        [JsonPropertyName("total_count")]
+        public int? TotalCount { get; set; }
+        [JsonPropertyName("count")]
+        public int? Count { get; set; }
+        [JsonPropertyName("page_number")]
+        public int? PageNumber { get; set; }
+        [JsonPropertyName("page_total")]
+        public int? PageTotal { get; set; }
+        [JsonPropertyName("limit")]
+        public int? Limit { get; set; }
+        [JsonPropertyName("txs")]
+        public List<TxResponse> Txs { get; set; }
+    }
+}
